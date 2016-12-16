af1c9000949f4b69989eb9e8798e1d32ae97c1bc
Karalkou <yury.karalkou@gmail.com>
2016-12-16 16:27:29 +0300

    Jump from chapters 1-3 to chapters 4..
    1. Removed files from first step (chapters 1-3)
    2. Added new files (server.js, app.js, login.js, logout.js)
    3. Modified (renewed) file 'webpack.config.js'

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,54 @@
+'use strict';
+
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
 
 module.exports = {
-    context: __dirname + '/frontend', // чтобы не дублировать в entry пути ./frontend/home, ./frontend/about etc
+    context: __dirname + '/frontend',
 
     entry: {
-        home: "./home",
-        about: "./about",
-        common: './common' // код отсюда плагин CommonChunksPlugin включит в результирующий файл common.js
+        app: './app'
     },
+
     output: {
-        path: __dirname + '/public', // рекомендуется использовать именно абсолютный путь
-        filename: '[name].js',
-        library: '[name]' // в переменной home  и about будут храниться экспортированные модули
+        path:     __dirname + '/public/js',
+        publicPath: '/js/',  //   /js/app.js
+        filename: "[name].js"
     },
 
     watch: NODE_ENV == 'development',
+
     watchOptions: {
         aggregateTimeout: 100
     },
 
-    devtool: NODE_ENV == 'development' ? 'source-map' : null,
-
     plugins: [
-        new webpack.NoErrorsPlugin(), // не позволяет webpack скомпилить файлы в случае ошибки
-        new webpack.DefinePlugin({
-            NODE_ENV: JSON.stringify(NODE_ENV)
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common',
-            minChunks: 2 // минимальное число модулей, для формирования чанков
-        }) // для выделения общ. части у модулей для возможности подключения на странице файла с общей частью + сам плагин можно вызывать несколько раз
+        new webpack.NoErrorsPlugin()
     ],
 
     resolve: {
-        moduleDirectories: ['node_modules'],
-        extensions: ['', '.js']
+        modulesDirectories: ['node_modules'],
+        extensions:         ['', '.js']
     },
 
     resolveLoader: {
-        moduleDirectories: ['node_modules'],
-        moduleTemplates: ['*-loader'],
-        extensions: ['', '.js']
-    },
-
-    module: {
-
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                presets: ['es2015']
-            }
-        }]
-
+        modulesDirectories: ['node_modules'],
+        moduleTemplates:    ['*-loader', '*'],
+        extensions:         ['', '.js']
     }
+
 };
 
-if(NODE_ENV == 'production'){
+
+if (NODE_ENV == 'production') {
     module.exports.plugins.push(
         new webpack.optimize.UglifyJsPlugin({
             compress: {
-                warnings: false,
+                // don't show unreachable variables etc
+                warnings:     false,
                 drop_console: true,
-                unsafe: true
+                unsafe:       true
             }
         })
-    )
+    );
 }
